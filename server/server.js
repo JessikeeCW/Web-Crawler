25a46e90c9b2dbb989d57d826b4fd990134f302e
Jessikee Campbell-Walker <jessikeecw@gmail.com>
2020-06-27 23:04:36 -0700

    BOILERPLATE
    This is the initial set up for my application. A client and server folder was set up.

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+const app = express()
+const PORT = process.env.port || 8000;
+
+app.use(bodyParser.json());
+
+//parse through the urlencoded body from the request and add it into the req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//static files go here
+app.use('/public', express.static(path.join(__dirname, '../client/public')));
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname,'../client/public/index.html'))
+})
+
+//require routes here
+
+
+//route handlers here
+
+
+//catches all unknown route request
+app.use((req, res) => {
+  console.log('in catch-all route handler');
+  res.sendStatus(404);
+})
+
+//global error handler
+app.use((req, res, err) => {
+  console.log(`global error handler caught unknown middleware error: ${err}`);
+  res.sendStatus(400).json({ err: 'an error has occured' })
+});
+
+app.listen(PORT, () => console.log(`listening on port:${PORT}`));
+
+module.exports = app;
