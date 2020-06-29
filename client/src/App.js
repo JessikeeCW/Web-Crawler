a3a7af704c6332b71860fd61c81df970dead73fd
Jessikee Campbell-Walker <jessikeecw@gmail.com>
2020-06-29 13:01:23 -0700

    SCRAPE DATA,DISPLAY DATA
    A frontend was set up to grab the data that was sent from the front end. This includes creating a layout o fhow the frontend is designed and forming the data to be sent to the server. The client folder also connects with the database allowing the data to be saved whenever the information is submitted. On the server side, a function was created to look through the html file to find all instaces of a tags. These url will be put into an array to be used for later.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Layout from './components/Layout';
+import Card from './components/Card';
+import fetch from 'isomorphic-unfetch';
 
 const App = () => {
-  return <div className="App">Checking</div>;
+  const [url, setUrl] = useState('');
+  const [limit, setLimit] = useState('');
+
+  const onChange = (e) => {
+    if (e.target.name === 'url') {
+      setUrl(e.target.value);
+    } else if (e.target.name === 'limit') {
+      setLimit(e.target.value);
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('http:///localhost:8000/search', {
+      method: 'POST',
+      headers: { 'Content-Type': 'Application/JSON' },
+      body: JSON.stringify({ url, limit }),
+    });
+    // const response = await res.json();
+    // setUrl('');
+    // setLimit('');
+  };
+
+  return (
+    <div className="App">
+      <Layout />
+
+      <form onSubmit={onSubmit}>
+        <div className="form-row mt-3 ml-3">
+          <div className="col-5">
+            <input
+              type="text"
+              name="url"
+              placeholder="Enter url here"
+              onChange={onChange}
+              className="form-control"
+            />
+          </div>
+          <div className="col-1">
+            <input
+              type="text"
+              name="limit"
+              placeholder="Enter limit"
+              onChange={onChange}
+              className="form-control"
+            />
+          </div>
+          <input type="submit" value="submit" className="btn btn-outline-info" />
+        </div>
+      </form>
+      <Card />
+    </div>
+  );
 };
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
-
-const app = express()
+const app = express();
 const PORT = process.env.port || 8000;
 
+app.use(cors({ origin: 'http://localhost:3000', methods: 'POST' }));
 app.use(bodyParser.json());
 
 //parse through the urlencoded body from the request and add it into the req.body
@@ -15,25 +16,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(path.join(__dirname, '../client/public')));
 
 app.get('/', (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname,'../client/public/index.html'))
-})
+  res.status(200).sendFile(path.resolve(__dirname, '../client/public/index.html'));
+});
 
 //require routes here
-
+const apiRouter = require('./routes/api');
 
 //route handlers here
-
+app.use('/', apiRouter);
 
 //catches all unknown route request
 app.use((req, res) => {
   console.log('in catch-all route handler');
   res.sendStatus(404);
-})
+});
 
 //global error handler
 app.use((req, res, err) => {
   console.log(`global error handler caught unknown middleware error: ${err}`);
-  res.sendStatus(400).json({ err: 'an error has occured' })
+  res.sendStatus(400).json({ err: 'an error has occured' });
 });
 
 app.listen(PORT, () => console.log(`listening on port:${PORT}`));
